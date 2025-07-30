f8cc3ea978b9249e9eb9bdad9cf6d2362a437e18
wx940703 <zhaoqi001@hotmail.com>
2025-07-30 22:48:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,4 +10,4 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 #修改容量
-sed -i 's/0xf80000/0xfd0000/g' target/linux/ath79/dts/ar7161_netgear_wndr3800.dts
+#sed -i 's/0xf80000/0xfd0000/g' target/linux/ath79/dts/ar7161_netgear_wndr3800.dts
